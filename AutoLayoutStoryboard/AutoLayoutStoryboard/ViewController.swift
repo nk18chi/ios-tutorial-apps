5e24afe99f78587d745aa5ac2e5b71b5edd41830
Naoki Mita <naoki.mita18@gmail.com>
2020-04-19 12:54:03 -0700

    Finish Assignment2

---
+++ AutoLayoutStoryboard/AutoLayoutStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutStoryboard
+//
+//  Created by Naoki Mita on 2020-04-19.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
