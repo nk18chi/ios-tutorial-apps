5d89976f61972346854f5bb1a1eb24b75e8fb2d7
Naoki Mita <naoki.mita18@gmail.com>
2020-04-22 22:09:53 -0700

    Finish ApplePie app

---
+++ ApplePie/ApplePie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Naoki Mita on 2020-04-22.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+      guessedLetters.append(letter)
+      if !word.contains(letter) {
+        incorrectMovesRemaining -= 1
+      }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
---
+++ ApplePie/ApplePie/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  ApplePie
+//
+//  Created by Naoki Mita on 2020-04-22.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    func updateGameState() {
+      if currentGame.incorrectMovesRemaining == 0 {
+        totalLosses += 1
+      } else if currentGame.word == currentGame.formattedWord {
+        totalWins += 1
+      } else {
+        updateUI()
+      }
+    }
+    
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newRound()
+    }
+    
+    var currentGame: Game!
+    
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool) {
+      for button in letterButtons {
+        button.isEnabled = enable
+      }
+    }
+    
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins), Losses:\(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+}
+
