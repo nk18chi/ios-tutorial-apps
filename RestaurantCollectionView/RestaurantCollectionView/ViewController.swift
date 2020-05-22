7a30134dc8d7d0cb4c0bb9e671e403cf5b404fb9
Naoki Mita <naoki.mita18@gmail.com>
2020-05-22 15:22:03 -0700

    Initilize RestaurantCollectView app

---
+++ RestaurantCollectionView/RestaurantCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantCollectionView
+//
+//  Created by Naoki Mita on 2020-05-22.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
